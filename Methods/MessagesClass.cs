328fdbcc572b0c3e8c66f6dc159a0686a2b34b4f
RechtDev <domjr452@hotmail.com>
2018-08-01 04:13:34 -0400

    Added class to the Messages and IpCalling to declare which .cs files were helper classes.

---
+++ Methods/MessagesClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IpFindIt
+{
+    static class Messages
+    {
+        internal static void AlertMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }      
+
+        internal static void PromptMessage(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        internal static void PromptMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
