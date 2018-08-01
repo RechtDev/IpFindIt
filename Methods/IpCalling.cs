6ab982c1dc0b1a037cf0f325b3128594d82323c8
RechtDev <domjr452@hotmail.com>
2018-08-01 02:59:23 -0400

    Initial project Version

---
+++ Methods/IpCalling.cs
@@ -0,0 +1,73 @@
+﻿using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+
+namespace IpFindIt
+{
+    class IpCalling
+    {
+        
+        internal static void CustomIpAddress(string IpAddress)
+        {
+            string IPGeolocationApiUrl;
+            IPGeolocationApiUrl = "http://ip-api.com/json/" + IpAddress;
+            WebRequest GetJsonApiFeedback;
+            GetJsonApiFeedback = WebRequest.Create(IPGeolocationApiUrl);
+            Stream ReturnedFeedBack;
+            ReturnedFeedBack = GetJsonApiFeedback.GetResponse().GetResponseStream();
+            StreamReader objReader = new StreamReader(ReturnedFeedBack);
+            string IpGeolocationFeedBack = objReader.ReadToEnd();
+            IpGeolocationMapping ipGeolocationMapping = JsonConvert.DeserializeObject<IpGeolocationMapping>(IpGeolocationFeedBack);
+            Messages.PromptMessage($"Here are the results for IP-Address:{IpAddress}\n");
+            Messages.PromptMessage("Results: \n");
+            Messages.PromptMessage($"Country: { ipGeolocationMapping.country}\n" +
+                                   $"State: {ipGeolocationMapping.regionName}\n" +
+                                   $"City: {ipGeolocationMapping.city}\n" +
+                                   $"Zip-Code: {ipGeolocationMapping.zip}\n" +
+                                   $"ISP: {ipGeolocationMapping.isp}", ConsoleColor.Magenta);
+
+            string ResolvedIps = String.Format("Country: {0,-20}" +
+                                               "State: {1,-20}" +
+                                               "City: {2,-20}" +
+                                               "Zip-Code: {3,-20}" +
+                                               "ISP: {4,-20}", ipGeolocationMapping.country, 
+                                                               ipGeolocationMapping.regionName, 
+                                                               ipGeolocationMapping.city, 
+                                                               ipGeolocationMapping.zip,
+                                                               ipGeolocationMapping.isp);
+
+            string UsersPcName = Environment.UserName;
+            
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@"C:\Users\" + UsersPcName + @"\Desktop\ResolvedIps.txt", true))
+            {
+                file.WriteLine($"{ResolvedIps}\n");
+
+            }
+        }
+
+
+    
+    }
+
+    public class IpGeolocationMapping
+    {
+        public string status { get; set; }
+        public string country { get; set; }
+        public string countryCode { get; set; }
+        public string region { get; set; }
+        public string regionName { get; set; }
+        public string city { get; set; }
+        public string zip { get; set; }
+        public string lat { get; set; }
+        public string lon { get; set; }
+        public string timezone { get; set; }
+        public string isp { get; set; }
+        public string org { get; set; }
+        public string _as { get; set; }
+        public string query { get; set; }
+    }
+
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IpFindIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BufferHeight = 42;           
+            Console.SetWindowSize(100, 42);
+            Messages.PromptMessage("Hello, Please enter your name.", ConsoleColor.Green);
+            string name = Console.ReadLine();
+            while (String.IsNullOrEmpty(name))
+            {
+                Messages.AlertMessage("Please enter your name!");
+                name = Console.ReadLine();
+
+            }
+            bool BackToMenu = true;
+            while (BackToMenu)
+            {
+                Console.WriteLine($"Hello {name}, welcome!");
+                Console.WriteLine($"Now, {name}, what would you like to do?\n");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("1)Find IP Addresse \n" +
+                                  "2)Activate Geo-location services \n" +
+                                  "3)Run GUI version of program");
+                string menuChoice = Console.ReadLine();
+                int menuNumber;
+                while (!int.TryParse(menuChoice, out menuNumber))
+                {
+                    if (menuNumber > 3 || menuNumber < 1)
+                    {
+                        Messages.AlertMessage(message: "You have entered an invalid Number, please select a correct option! (1-3)");
+                        menuChoice = Console.ReadLine();
+                    }
+                    else if (menuNumber == default)
+                    {
+                        Messages.AlertMessage("You have not entered anything! Please enter a choice of either 1, 2, or 3.");
+                        menuChoice = Console.ReadLine();
+
+                    }
+                }
+                bool repeat = true;
+                while (repeat)
+                {
+                    switch (Int32.Parse(menuChoice))
+                    {
+                        case 1:
+                            Console.Clear();
+                            Messages.PromptMessage("Okay please enter the IP Addresse you wish you look up.", ConsoleColor.Green);
+                            Messages.PromptMessage("IP Address: ", ConsoleColor.Green);
+                            string IpAddress = Console.ReadLine();
+                            IpCalling.CustomIpAddress(IpAddress);
+                            
+                            Messages.PromptMessage("Would you like to search another IP (Y/N)", ConsoleColor.Green);
+                            string goOn = Console.ReadLine().ToUpper(); ;
+
+                            while (String.IsNullOrEmpty(goOn))
+                            {
+                                Messages.AlertMessage("Please choose either (Y)es or (N)o");
+                                goOn = Console.ReadLine().ToUpper();
+                                
+                            }
+                            if (goOn == "Y")
+                            {
+                                continue;
+                            }
+                            if (goOn == "N")
+                            {
+                                Console.Clear();
+                                repeat = false;
+                                BackToMenu = true;
+    
+                            }                            
+                            break;
+
+                    }
+                }
+            }
+        }
+        
+        static void ListItem()
+        {
+            
+        }       
+    }
+}
